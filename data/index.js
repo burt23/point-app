a171497fdb28f302d59e462fd1e7d198cd339b72
Michael Burton <mail4mdb@gmail.com>
2021-04-01 11:17:08 -0500

    add mock data for users and posts

---
+++ data/index.js
@@ -0,0 +1,61 @@
+const users = [
+  {
+    email: "test@gmail.com",
+    password: "superTopSecret",
+  },
+  {
+    email: "tst@gmil.com",
+    password: "suprTopSecret",
+  },
+  {
+    email: "test@gil.com",
+    password: "supTopSecret",
+  },
+  {
+    email: "test@gl.com",
+    password: "supTopSecret",
+  },
+  {
+    email: "tet@gail.com",
+    password: "suprSecret",
+  },
+];
+
+const posts = [
+  {
+    userId: 1,
+    title: "awesome post!",
+    body: "dont you love javascript!?",
+  },
+  {
+    userId: 2,
+    title: "another great idea",
+    body: "will webassembly beat out JS in the future?",
+  },
+
+  {
+    userId: 3,
+    title: "what about rust?",
+    body: "dont we love type safe code? :)",
+  },
+
+  {
+    userId: 4,
+    title: "functional for lyfe",
+    body: "I hope you didn't forget about HaskelL!!",
+  },
+
+  {
+    userId: 5,
+    title: "do it in GO",
+    body: "you know that google made this right??",
+  },
+
+  {
+    userId: 1,
+    title: "NooB Question",
+    body: "how should I get started into coding?",
+  },
+];
+
+module.exports = { users, posts };
