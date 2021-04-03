2d165ea5aedb7524874d2ecd45982efee9491b4e
Michael Burton <mail4mdb@gmail.com>
2021-04-03 11:33:03 -0500

    mock out global config folder

--- dataSources/PointAppAPI.js
+++ dataSources/PointAppAPI.js
@@ -1,19 +1,21 @@
 const { RESTDataSource } = require("apollo-datasource-rest");
 
+const BASE_URL = require("../config");
+
 class PointAppAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = "https://pointapp.herokuapp.com/";
+    this.baseURL = BASE_URL;
   }
 
-  async getMovie(id) {
-    return this.get(`movies/${id}`);
+  async getUser(id) {
+    return this.get(`user/${id}`);
   }
 
-  async getMostViewedMovies(limit = 10) {
-    const data = await this.get("movies", {
+  async getUsers(limit = 10) {
+    const data = await this.get("user", {
       per_page: limit,
-      order_by: "most_viewed",
+      //   order_by: "most_viewed", // TODO: explore filtering options
     });
     return data.results;
   }
