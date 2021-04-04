9142b20ad9d37b481878b0789f279d19a7768757
Michael Burton <mail4mdb@gmail.com>
2021-04-04 16:16:50 -0500

    add auth0 and express auth dependencies, sending user through auth0 SSO

--- server/index.js
+++ server/index.js
@@ -2,8 +2,22 @@ const { ApolloServer } = require("apollo-server");
 const resolvers = require("../graphql/resolvers");
 const PointAppAPI = require("../dataSources/PointAppAPI");
 const typeDefs = require("../graphql/typeDefs");
+const express = require("express");
+const { auth } = require("express-openid-connect");
 
+const app = express();
+
+const EXPRESS_PORT = 3000;
 const PORT = process.env.PORT || 8000;
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "a long, randomly-generated string stored in env",
+  baseURL: "http://localhost:3000",
+  clientID: "N1MUWRz21k3QMvUmji4DKxnXt6CNdNKX",
+  issuerBaseURL: "https://dev-0c5cv0t2.us.auth0.com",
+};
 
 const server = new ApolloServer({
   typeDefs,
@@ -12,6 +26,32 @@ const server = new ApolloServer({
   context: () => ({ token: "foobarboobaz" }), // TODO: testing for auth
   playground: true,
   introspection: true,
+});
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config), (req, res, next) => {
+  console.log("inside auth middleware, logged in?", req.oidc.isAuthenticated());
+  const { details: { body } = { body: "" } } = req;
+  console.log("user infO??", body);
+  if (req.oidc.isAuthenticated()) {
+    res.redirect("http://localhost:8000");
+  }
+  next();
+});
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  console.log("hit home route");
+  // res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+  if (req.oidc.isAuthenticated()) {
+    res.redirect("http://localhost:8000");
+  } else {
+    res.redirect("http://localhost:3000/login");
+  }
+});
+
+app.listen(EXPRESS_PORT, () => {
+  console.log("express up and running");
 });
 
 server.listen(PORT).then(({ url }) => {
